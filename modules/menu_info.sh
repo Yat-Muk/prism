848d057d9c58a640b03a9de3106e15342bee6a31
Yat-Muk <104666586+Yat-Muk@users.noreply.github.com>
2025-12-04 14:14:23 +0800

    Update menu_info.sh

--- modules/menu_info.sh
+++ modules/menu_info.sh
@@ -33,7 +33,7 @@ p_kv() {
     local key="$1"
     local val="$2"
     
-    local width=22
+    local width=20
     
     local padding=$(awk -v str="$key" -v w="$width" 'BEGIN {
         len = length(str);
